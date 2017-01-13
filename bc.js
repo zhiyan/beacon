8c452204f102bf4b364ff970882bf1a875c7108c
zhiyan <zhiyan1987@gmail.com>
2017-01-13 14:02:05 +0800

    remove uv send

--- bc.js
+++ bc.js
@@ -21,7 +21,7 @@ function Beacon(){
     if(this.initOption.uv){
         if(!this.cookie('_utrace')){
             this.cookie('_utrace', this.uuid(), {expires:1, domain:'.daikuan.com'})
-            this.send('uv')
+            // this.send('uv')
         }
     }
 
